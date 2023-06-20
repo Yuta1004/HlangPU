d6fce719d9dae9036534b45dcc58dd3c26120cf6
NakagamiYuta <guguru0014@gmail.com>
2023-06-21 02:33:30 +0900

    [add] 数値の認識を行えるようにした

--- ClangPU.ip/clangpu_1_0/hdl/lexer.v
+++ ClangPU.ip/clangpu_1_0/hdl/lexer.v
@@ -11,8 +11,19 @@ module lexer
         output reg  [15:0]  O_DATA
     );
 
+    function [7:0] x10add;
+        input [7:0] a;
+        input [7:0] b;
+        if (a != 8'hff && b >= 8'h30 && b <= 8'h39)
+            x10add = (a << 3) + (a << 1) + (b - 8'h30);
+        else
+            x10add = 8'hff;
+    endfunction
+
     /* ----- 1. 空白文字消去 ----- */
-    reg [7:0] str_8x8 [0:7];
+    reg [7:0]   str_8x8 [0:7];
+    reg [63:0]  str_64;
+    reg [7:0]   num_8   [0:1];
 
     always @ (posedge CLK) begin
         if (RST) begin
@@ -24,20 +35,24 @@ module lexer
             str_8x8[2] <= 8'b0;
             str_8x8[1] <= 8'b0;
             str_8x8[0] <= 8'b0;
+            str_64 <= 64'b0;
+            num_8[1] <= 8'b0;
+            num_8[0] <= 8'b0;
         end
         else if(I_VALID) begin
             if (
                 I_DATA == 8'h00 || I_DATA == 8'hff ||                   // EOF
                 I_DATA == 8'h09 || I_DATA == 8'h0a || I_DATA == 8'h20   // ' ', '\n', '\t'
             ) begin
-                str_64_valid <= 1'b1;
                 str_64 <= {
                     str_8x8[7][7:0], str_8x8[6][7:0], str_8x8[5][7:0], str_8x8[4][7:0],
                     str_8x8[3][7:0], str_8x8[2][7:0], str_8x8[1][7:0], str_8x8[0][7:0]
                 };
+                num_8[1] <= num_8[0] == 8'hff ? 8'b0 : num_8[0];
+                num_8[0] <= 8'b0;
             end
             else begin
-                str_64_valid <= 1'b0;
+                str_64 <= 64'b0;
                 str_8x8[7] <= str_8x8[6];
                 str_8x8[6] <= str_8x8[5];
                 str_8x8[5] <= str_8x8[4];
@@ -46,14 +61,13 @@ module lexer
                 str_8x8[2] <= str_8x8[1];
                 str_8x8[1] <= str_8x8[0];
                 str_8x8[0] <= I_DATA;
+                num_8[0] <= x10add(num_8[0], I_DATA);
             end
         end
     end
 
     /* ----- 2. 文字列比較 ----- */
-    reg         str_64_valid;
-    reg [63:0]  str_64;
-
+    parameter NUM       = 8'd0;
     parameter CHAR      = 8'd1;
     parameter FOR       = 8'd2;
     parameter WHILE     = 8'd3;
@@ -61,17 +75,14 @@ module lexer
     assign O_VALID = O_DATA != 16'b0;
 
     always @ (posedge CLK) begin
-        if (RST) begin
-            str_64_valid <= 1'b0;
-            str_64 <= 64'b0;
-            O_DATA <= 1'b0;
-        end
-        else if (str_64_valid && O_DATA == 16'b0)
+        if (RST)
+            O_DATA <= 16'b0;
+        else if (str_64 != 64'b0 && O_DATA == 16'b0)
             casex (str_64)
-                64'hxx_xx_xx_xx_63_68_61_72: O_DATA <= { CHAR, 8'b0 };     // "char"
-                64'hxx_xx_xx_xx_xx_66_6f_72: O_DATA <= { FOR, 8'b0 };      // "for"
-                64'hxx_xx_xx_77_68_69_6c_65: O_DATA <= { WHILE, 8'b0 };    // "while"
-                default:                     O_DATA <= 16'b0;              // Unknown Token
+                64'hxx_xx_xx_xx_63_68_61_72: O_DATA <= { CHAR, 8'b0 };      // "char"
+                64'hxx_xx_xx_xx_xx_66_6f_72: O_DATA <= { FOR, 8'b0 };       // "for"
+                64'hxx_xx_xx_77_68_69_6c_65: O_DATA <= { WHILE, 8'b0 };     // "while"
+                default:                     O_DATA <= { NUM, num_8[1] };   // Unknown Tag or NUM
             endcase
         else
             O_DATA <= 16'b0;
--- ClangPU.ip/clangpu_1_0/tb/test.c
+++ ClangPU.ip/clangpu_1_0/tb/test.c
@@ -1,8 +1,8 @@
 for while char
 
 int main() {
-    int a = 10;
-    int b = 20;
+    int a = 10 ;
+    int b = 20 ;
     int c = a + b;
     return c;
 }
