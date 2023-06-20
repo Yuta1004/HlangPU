1863b0b753fc132b28bcc1ee90ce8429f39a782b
NakagamiYuta <guguru0014@gmail.com>
2023-06-20 18:18:41 +0900

    [clean] clangpu.v

--- ClangPU.ip/clangpu_1_0/hdl/clangpu_v1_0.v
+++ ClangPU.ip/clangpu_1_0/hdl/clangpu_v1_0.v
@@ -11,13 +11,13 @@ module core #
         parameter integer C_M_AXI_RUSER_WIDTH     = 4,
         parameter integer C_M_AXI_BUSER_WIDTH     = 1
     )
-
     (
-        // AXI Bus Signals
+        /* ----- AXIバス ----- */
+        // クロック・リセット
         input wire          ACLK,
         input wire          ARESETN,
 
-        // Master Interface Write Address
+        // AWチャネル
         output wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_AXI_AWID,
         output wire [C_M_AXI_ADDR_WIDTH-1:0]        M_AXI_AWADDR,
         output wire [8-1:0]                         M_AXI_AWLEN,
@@ -26,14 +26,12 @@ module core #
         output wire [2-1:0]                         M_AXI_AWLOCK,
         output wire [4-1:0]                         M_AXI_AWCACHE,
         output wire [3-1:0]                         M_AXI_AWPROT,
-        // AXI3 output wire [4-1:0]                  M_AXI_AWREGION,
         output wire [4-1:0]                         M_AXI_AWQOS,
         output wire [C_M_AXI_AWUSER_WIDTH-1:0]      M_AXI_AWUSER,
         output wire                                 M_AXI_AWVALID,
         input  wire                                 M_AXI_AWREADY,
 
-        // Master Interface Write Data
-        // AXI3 output wire [C_M_AXI_THREAD_ID_WIDTH-1:0]     M_AXI_WID,
+        // Wチャネル
         output wire [C_M_AXI_DATA_WIDTH-1:0]        M_AXI_WDATA,
         output wire [C_M_AXI_DATA_WIDTH/8-1:0]      M_AXI_WSTRB,
         output wire                                 M_AXI_WLAST,
@@ -41,14 +39,14 @@ module core #
         output wire                                 M_AXI_WVALID,
         input  wire                                 M_AXI_WREADY,
 
-        // Master Interface Write Response
+        // Bチャネル
         input  wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_AXI_BID,
         input  wire [2-1:0]                         M_AXI_BRESP,
         input  wire [C_M_AXI_BUSER_WIDTH-1:0]       M_AXI_BUSER,
         input  wire                                 M_AXI_BVALID,
         output wire                                 M_AXI_BREADY,
 
-        // Master Interface Read Address
+        // ARチャネル
         output wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_AXI_ARID,
         output wire [C_M_AXI_ADDR_WIDTH-1:0]        M_AXI_ARADDR,
         output wire [8-1:0]                         M_AXI_ARLEN,
@@ -57,13 +55,12 @@ module core #
         output wire [2-1:0]                         M_AXI_ARLOCK,
         output wire [4-1:0]                         M_AXI_ARCACHE,
         output wire [3-1:0]                         M_AXI_ARPROT,
-        // AXI3 output wire [4-1:0]                  M_AXI_ARREGION,
         output wire [4-1:0]                         M_AXI_ARQOS,
         output wire [C_M_AXI_ARUSER_WIDTH-1:0]      M_AXI_ARUSER,
         output wire                                 M_AXI_ARVALID,
         input  wire                                 M_AXI_ARREADY,
 
-        // Master Interface Read Data
+        // Rチャネル
         input  wire [C_M_AXI_THREAD_ID_WIDTH-1:0]   M_AXI_RID,
         input  wire [C_M_AXI_DATA_WIDTH-1:0]        M_AXI_RDATA,
         input  wire [2-1:0]                         M_AXI_RRESP,
@@ -72,13 +69,11 @@ module core #
         input  wire                                 M_AXI_RVALID,
         output wire                                 M_AXI_RREADY,
 
-        // CPU制御
+        /* ----- CPU制御用 ----- */
         input wire          CCLK,
         input wire          CRST,
         input wire          CEXEC,
         input wire  [31:0]  CMEM_ADDR,
-
-        // CPU状態
         output wire         CSTAT
     );
 
@@ -122,17 +117,6 @@ module core #
     // Rチャネル
     assign M_AXI_RREADY  = 1'b0;    // *
 
-    // assign CSTAT = 1'b1;
-
-    reg stat;
-
-    assign CSTAT = stat;
-
-    always @(posedge CCLK) begin
-        if (CRST)
-            stat <= 1'b0;
-        else if (CEXEC)
-            stat <= 1'b1;
-    end
+    assign CSTAT = 1'b1;
 
 endmodule
