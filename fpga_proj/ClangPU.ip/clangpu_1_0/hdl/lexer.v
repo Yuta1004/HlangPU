67fa3aa6ec6cd6a16a6ff68b72b8e8bcf20e2967
NakagamiYuta <guguru0014@gmail.com>
2023-06-23 02:57:55 +0900

    [fix] 数値 0 を正しく認識できるようにした

--- fpga_proj/ClangPU.ip/clangpu_1_0/hdl/lexer.v
+++ fpga_proj/ClangPU.ip/clangpu_1_0/hdl/lexer.v
@@ -23,6 +23,11 @@ module lexer
             x10add = 8'hff;
     endfunction
 
+    function [15:0] num_or_unknown;
+        input [7:0] a;
+        num_or_unknown = a != 8'hff ? { NUM, a } : 16'hffff;
+    endfunction
+
     /* ----- 1. 空白文字消去 ----- */
     reg [7:0]   str_8x8 [0:7];
     reg [63:0]  str_64;
@@ -51,7 +56,7 @@ module lexer
                     str_8x8[7][7:0], str_8x8[6][7:0], str_8x8[5][7:0], str_8x8[4][7:0],
                     str_8x8[3][7:0], str_8x8[2][7:0], str_8x8[1][7:0], str_8x8[0][7:0]
                 };
-                num_8[1] <= num_8[0] == 8'hff ? 8'b0 : num_8[0];
+                num_8[1] <= num_8[0];
                 num_8[0] <= 8'b0;
             end
             else begin
@@ -70,19 +75,49 @@ module lexer
     end
 
     /* ----- 2. 文字列比較 ----- */
-    reg [63:0] o_data_ready;
+    reg [15:0] o_data_ready;
 
-    `include "init_tokens.vh"
+    parameter NUM       = 8'h00;    // NUM
+    parameter OUT       = 8'h01;    // "out"
+    parameter VAR_A     = 8'h02;    // "a"
+    parameter EQUAL     = 8'h03;    // "="
+    parameter VAR_B     = 8'h04;    // "b"
+    parameter VAR_C     = 8'h05;    // "c"
+    parameter IF        = 8'h06;    // "if"
+    parameter BRACKET_A = 8'h07;    // "("
+    parameter BRACKET_B = 8'h08;    // ")"
+    parameter PLUS      = 8'h09;    // "+"
+    parameter MINUS     = 8'h0a;    // "-"
+    parameter SEMICOLON = 8'h0b;    // ";"
+    parameter EOF       = 8'h0c;    // "EOF"
+
+    always @* begin
+        casex (str_64)
+            64'hxx_xx_xx_xx_xx_xx_xx_61: o_data_ready <= { VAR_A, 8'b0 };           // "a"
+            64'hxx_xx_xx_xx_xx_xx_xx_62: o_data_ready <= { VAR_B, 8'b0 };           // "b"
+            64'hxx_xx_xx_xx_xx_xx_xx_63: o_data_ready <= { VAR_C, 8'b0 };           // "c"
+            64'hxx_xx_xx_xx_xx_xx_xx_28: o_data_ready <= { BRACKET_A, 8'b0 };       // "("
+            64'hxx_xx_xx_xx_xx_xx_xx_29: o_data_ready <= { BRACKET_B, 8'b0 };       // ")"
+            64'hxx_xx_xx_xx_xx_xx_xx_3d: o_data_ready <= { EQUAL, 8'b0 };           // "="
+            64'hxx_xx_xx_xx_xx_xx_xx_2b: o_data_ready <= { PLUS, 8'b0 };            // "+"
+            64'hxx_xx_xx_xx_xx_xx_xx_2d: o_data_ready <= { MINUS, 8'b0 };           // "-"
+            64'hxx_xx_xx_xx_xx_xx_xx_3b: o_data_ready <= { SEMICOLON, 8'b0 };       // "-"
+            64'hxx_xx_xx_xx_xx_xx_69_66: o_data_ready <= { IF, 8'b0 };              // "if"
+            64'hxx_xx_xx_xx_xx_6f_75_74: o_data_ready <= { OUT, 8'b0 };             // "out"
+            64'hxx_xx_xx_xx_xx_45_4f_46: o_data_ready <= { EOF, 8'b0 };             // "eof"
+            default:                     o_data_ready <= num_or_unknown(num_8[1]);  // NUM or Unknown
+        endcase
+    end
 
     always @ (posedge CLK) begin
         if (RST) begin
             FOUND_EOF <= 1'b0;
             O_VALID <= 1'b0;
-            O_DATA <= 64'b0;
+            O_DATA <= 16'b0;
         end
         else begin
             FOUND_EOF <= FOUND_EOF | o_data_ready[15:8] == EOF;
-            O_VALID <= (o_data_ready != 64'b0) && (o_data_ready != O_DATA);
+            O_VALID <= (o_data_ready != 16'hffff) && (o_data_ready != O_DATA);
             O_DATA <= o_data_ready;
         end
     end
--- fpga_proj/ClangPU.ip/clangpu_1_0/tb/test.c
+++ fpga_proj/ClangPU.ip/clangpu_1_0/tb/test.c
@@ -4,4 +4,9 @@ b = 20 ;
 c = 30 ;
 if ( 30 - 20 + 10 ) a = 30 ;
 out 10 ;
+out 0 ;
+out 20 ;
+out 0 ;
+out 30 ;
+out 0 ;
 EOF
